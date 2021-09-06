6d188f1fd0825e3a39d108790a0eb88a32e04666
WaTerSp0uT <ericyarek@gmail.com>
2021-09-06 14:52:20 -0400

    refactored

--- src/javapractice/CountWords.java
+++ src/javapractice/CountWords.java
@@ -6,32 +6,29 @@ import java.util.Map;
 public class CountWords {
     public static void main(String[] args) {
 
-
         countWords("  Hello    myName   is dasd  t  is Hello   dasd  ");
     }
 
-
-
-    public static void countWords (String strInput){
+    public static void countWords(String strInput) {
         //splitting to an array
         String[] arrayOfStrings = strInput.trim().split("\\s+");
 
         //Putting into the map with keys and counting it
-        Map<String,Integer> mapOfWords = new HashMap<>();
+        Map<String, Integer> mapOfWords = new HashMap<>();
 
-        for(int i=0; i<arrayOfStrings.length; i++){
-            if(!mapOfWords.containsKey(arrayOfStrings[i])){
+        for (int i = 0; i < arrayOfStrings.length; i++) {
+            //if key does not exist create one
+            if (!mapOfWords.containsKey(arrayOfStrings[i])) {
                 mapOfWords.put(arrayOfStrings[i], 1);
-            }else {
+            } else {
+                //if exist add Up the count of word
                 mapOfWords.put(arrayOfStrings[i], mapOfWords.get(arrayOfStrings[i]) + 1);
             }
         }
-
-        System.out.println("There are "+ arrayOfStrings.length + " of words");
-        for(String map : mapOfWords.keySet()){
-            System.out.println("Then count for word:"+ map +" is "+ mapOfWords.get(map));
+        System.out.println("There are " + arrayOfStrings.length + " of words");
+        for (String map : mapOfWords.keySet()) {
+            System.out.println("Then count for word:" + map + " is " + mapOfWords.get(map));
         }
-
 
     }
 }
